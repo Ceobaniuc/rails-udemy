813862fa9d901935f1c5de9be5221a5d7a33d546
Pedro Henrique Alves Mourão Ceobaniuc <017833032356@rrows18242.tre-ro.jus.br>
2019-04-02 16:19:36 -0400

    Integrated technologies model

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class PortfoliosController < ApplicationController
   def index
     @portfolio_item = Portfolio.all
@@ -11,49 +13,49 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.new
   end
 
-def create
-  @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
-  respond_to do |format|
-    if @portfolio_item.save
-      format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live' }
-    else
-      format.html { render :new}
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live' }
+      else
+        format.html { render :new }
+     end
+    end
    end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
   end
- end
 
-def edit
-  @portfolio_item = Portfolio.find(params[:id])
-end
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
 
-def update
-  @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'The record succesfully update' }
+      else
+        format.html { render :edit }
+      end
+    end
+   end
 
-  respond_to do |format|
-    if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body,))
-      format.html {redirect_to portfolios_path, notice: "The record succesfully update"}
-    else
-      format.html { render :edit }
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def destroy
+    # Perform the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+
+    # Destroy/delete the record
+    @portfolio_item.destroy
+
+    # Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
+      format.json { head :no_content }
     end
   end
- end
-
-def show
-  @portfolio_item = Portfolio.find(params[:id])
-end
-
-def destroy
-  # Perform the lookup
-  @portfolio_item = Portfolio.find(params[:id])
-
-  # Destroy/delete the record
-  @portfolio_item.destroy
-
-  # Redirect
-  respond_to do |format|
-    format.html { redirect_to portfolios_url, notice: "Record was removed." }
-    format.json { head :no_content }
-  end
-end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -46,3 +46,12 @@ Portfolio.create!(
 end
 
 puts "9 portfolios items created"
+
+3.times do |technology|
+  Technology.create!(
+    name: "Technology #{technology}",
+    portfolio_id: Portfolio.last.id
+  )
+end
+
+puts "3 technologies created"
