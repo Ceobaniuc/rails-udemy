e11dc51e0906b55c8fb833f08aa920afd342d10a
Pedro Henrique Alves Mourão Ceobaniuc <017833032356@rrows18242.tre-ro.jus.br>
2019-04-16 18:18:16 -0400

    Implemented collection based partials

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,7 +4,7 @@ class PortfoliosController < ApplicationController
   layout 'portfolio'
 
   def index
-    @portfolio_item = Portfolio.all
+    @portfolio_items = Portfolio.all
   end
 
   def angular
